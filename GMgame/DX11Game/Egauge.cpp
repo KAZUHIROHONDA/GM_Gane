aa5ff859937a3ec4da6f57285836a566c2c36141
yoshiki44 <matsuiyoshiki44@gmail.com>
2023-06-19 10:11:09 +0900

    ゲージ追加

--- GMgame/DX11Game/Egauge.cpp
+++ GMgame/DX11Game/Egauge.cpp
@@ -3,18 +3,18 @@
 // ゲージ処理 [gauge.cpp]
 //
 //=============================================================================
-#include "gauge.h"
+#include "Egauge.h"
 #include "polygon.h"	// ポリゴン使用
 #include "Texture.h"
 
 // マクロ定義
-#define GAUGE_TEXTURENAME	_T("data/texture/gauge000.png")
-#define GAUGE_SIZE_X	(256)	// サイズX
-#define GAUGE_SIZE_Y	(32)	// サイズY
-
-#define GAUGE1_TEXTURENAME	_T("data/texture/gaugeframe.png")
-#define GAUGE1_SIZE_X	(256)	// サイズX
-#define GAUGE1_SIZE_Y	(32)	// サイズY
+#define EGAUGE_TEXTURENAME	_T("data/texture/gauge001.png")
+#define EGAUGE_SIZE_X	(256)	// サイズX
+#define EGAUGE_SIZE_Y	(32)	// サイズY
+	    
+#define EGAUGE1_TEXTURENAME	_T("data/texture/gaugeframe.png") 
+#define EGAUGE1_SIZE_X	(256)	// サイズX
+#define EGAUGE1_SIZE_Y	(32)	// サイズY
 
 // 構造体定義
 // プロトタイプ宣言
@@ -23,19 +23,19 @@ static ID3D11ShaderResourceView*	g_pTexture;				// テクスチャ
 static ID3D11ShaderResourceView*	g_pTexture1;				// テクスチャ
 
 // 初期化処理
-HRESULT InitGauge( void )
+HRESULT InitEGauge( void )
 {
 	// テクスチャ読み込み
 	HRESULT hr = CreateTextureFromFile(GetDevice(),
-		GAUGE_TEXTURENAME, &g_pTexture);
+		EGAUGE_TEXTURENAME, &g_pTexture);
 	// テクスチャ読み込み
 	      hr = CreateTextureFromFile(GetDevice(),
-		GAUGE1_TEXTURENAME, &g_pTexture1);
+		EGAUGE1_TEXTURENAME, &g_pTexture1);
 
 	return hr;
 }
 
-void UninitGauge( void )
+void UninitEGauge( void )
 {
 	// テクスチャ開放
 	SAFE_RELEASE(g_pTexture);
@@ -43,14 +43,14 @@ void UninitGauge( void )
 	SAFE_RELEASE(g_pTexture1);
 }
 
-void UpdateGauge( void )
+void UpdateEGauge( void )
 {
 	// １フレームごとの更新処理
 }
 
 //in:pos:左端座標指定
 //　	persent:100%を1.0として受け取る
-void DrawGauge(XMFLOAT2 pos, float percent)
+void DrawEGauge(XMFLOAT2 pos, float percent)
 {
 	//描画用にデバイスコンテキストを取得
 	ID3D11DeviceContext* pDeviceContext = GetDeviceContext();
@@ -59,8 +59,8 @@ void DrawGauge(XMFLOAT2 pos, float percent)
 
 	//設定
 	SetPolygonTexture(g_pTexture);
-	SetPolygonSize(GAUGE_SIZE_X * percent, GAUGE_SIZE_Y);
-	SetPolygonPos(pos.x - GAUGE_SIZE_X * percent / 2, pos.y);
+	SetPolygonSize(EGAUGE_SIZE_X * percent, EGAUGE_SIZE_Y);
+	SetPolygonPos(pos.x + EGAUGE_SIZE_X * percent / 2, pos.y);
 	SetPolygonUV(0.0f, 0.0f);
 	SetPolygonFrameSize(percent, 1.0f);
 
@@ -70,8 +70,8 @@ void DrawGauge(XMFLOAT2 pos, float percent)
 	SetPolygonFrameSize(1.0f,1.0f);
 	//設定
 	SetPolygonTexture(g_pTexture1);
-	SetPolygonSize(GAUGE1_SIZE_X , GAUGE1_SIZE_Y);
-	SetPolygonPos(-252.0f, 300.0f);
+	SetPolygonSize(EGAUGE1_SIZE_X , EGAUGE1_SIZE_Y);
+	SetPolygonPos(252.0f, 300.0f);
 	SetPolygonUV(0.0f, 0.0f);
 
 	
--- GMgame/DX11Game/enemy.cpp
+++ GMgame/DX11Game/enemy.cpp
@@ -14,19 +14,23 @@
 #include"input.h"
 #include"enemyhp.h"
 #include"fade.h"
-
+#include "Egauge.h"
 //*****************************************************************************
 // マクロ定義
 //*****************************************************************************
-#define MODEL_ENEMY		"data/model/kamereon.obj"
-//#define MODEL_ENEMY		"data/model/wani.obj"
-#define MODEL_ENEMY01	"data/model/enemy01.fbx"
-#define MODEL_ENEMY02	"data/model/enemy02.fbx"
+#define MODEL_ENEMY		"data/model/wani2.obj"
+#define MODEL_ENEMY1	"data/model/wani1.obj"
+#define MODEL_ENEMY2	"data/model/wani3.obj"
+#define MODEL_ENEMY3	"data/model/wani4.obj"
 
 #define VALUE_MOVE		(0.1f)		
 #define VALUE_ROTATE	(2.0f)
 
 #define ENEMY_RADIUS  (10.0f)
+
+#define ENEMY_EGAUGE_POS_X	(125.0f)
+#define ENEMY_EGAUGE_POS_Y	(300.0f)
+
 
 //構造体定義
 typedef struct _tEnemy
@@ -41,9 +45,10 @@ typedef struct _tEnemy
 	int				nPhase;			//段階
 	int				nState;			//状態(0:未使用　1:通常)
 	int				Tipe;
+	int				nStopTime;		//操作不能時間
 
 	int				nHP;
-
+	int				nEGauge;
 	int				nShadowIdx;		//影の番号
 
 }tEnemy;
@@ -51,8 +56,18 @@ typedef struct _tEnemy
 //*****************************************************************************
 // グローバル変数
 //*****************************************************************************
-static CAssimpModel		g_model[3];				//モデルデータ格納用
+static CAssimpModel		g_model[4];				//モデルデータ格納用
 static tEnemy			g_enemy[ENEMY_MAX];	//プレイヤー
+static tEnemy			g_enemyHD[ENEMY_MAX];
+static tEnemy			g_enemyAM[ENEMY_MAX];
+static tEnemy			g_enemyLG[ENEMY_MAX];
+
+
+static int			g_nCnt1;//行動カウント1コメ
+static int			g_nCnt2;//行動カウント2コメ
+static int			g_nCnt3;//行動カウント3コメ
+static int			g_nCnt4;//行動カウント4コメ
+static bool				g_atama = true;//false;
 
 int					bfCnt;
 
@@ -68,18 +83,55 @@ HRESULT InitEnemy(void)
 	// 位置・回転・スケールの初期設定
 	for (int i = 0; i < ENEMY_MAX; i++)
 	{
-		g_enemy[i].pos = XMFLOAT3(0.0f, 0.0f, 200.0f);
+		g_enemy[i].pos = XMFLOAT3(0.0f, 20.0f, 20.0f);
 		g_enemy[i].rot = XMFLOAT3(0.0f, 180.0f, 0.0f);
-		g_enemy[i].scl = XMFLOAT3(3.0f,3.0f,3.0f);
+		g_enemy[i].scl = XMFLOAT3(8.0f, 8.0f, 8.0f);
 		g_enemy[i].vel = XMFLOAT3(0.0f, 0.0f, 0.0f);
 		//初期化
-		g_enemy[i].nPhase = 1;
-		g_enemy[i].nState = 0;		//最初は通常
+		g_enemy[i].nPhase = 0;
+		g_enemy[i].nState = 1;		//最初は通常
 		g_enemy[i].Tipe = 0;
 
 		g_enemy[i].nHP = 1000;
-
+		g_enemy[i].nEGauge = g_enemy[i].nHP / 10;
 		g_enemy[i].nShadowIdx = -1;
+
+
+		//頭
+		g_enemyHD[i].pos = XMFLOAT3(0.0f, 0.0f, -3.0f);
+		g_enemyHD[i].rot = XMFLOAT3(0.0f, 0.0f, 0.0f);
+		g_enemyHD[i].scl = XMFLOAT3(1.0f, 1.0f, 1.0f);
+		g_enemyHD[i].vel = XMFLOAT3(0.0f, 0.0f, 0.0f);
+		g_enemyHD[i].nPhase = 0;
+		g_enemyHD[i].nState = 1;	// puroぺらは最初から登場している
+		g_enemyHD[i].nShadowIdx = -1;
+
+		//腕
+		g_enemyAM[i].pos = XMFLOAT3(0.0f, -0.8f, -1.0f);
+		g_enemyAM[i].rot = XMFLOAT3(0.0f, 0.0f, 0.0f);
+		g_enemyAM[i].scl = XMFLOAT3(1.0f, 1.0f, 1.0f);
+		g_enemyAM[i].vel = XMFLOAT3(0.0f, 0.0f, 0.0f);
+		g_enemyAM[i].nPhase = 0;
+		g_enemyAM[i].nState = 1;	// puroぺらは最初から登場している
+		g_enemyAM[i].nShadowIdx = -1;
+
+		//足
+		g_enemyLG[i].pos = XMFLOAT3(0.0f, -0.8f, 0.0f);
+		g_enemyLG[i].rot = XMFLOAT3(0.0f, 0.0f, 0.0f);
+		g_enemyLG[i].scl = XMFLOAT3(1.0f, 1.0f, 1.0f);
+		g_enemyLG[i].vel = XMFLOAT3(0.0f, 0.0f, 0.0f);
+		g_enemyLG[i].nPhase = 0;
+		g_enemyLG[i].nState = 1;	// puroぺらは最初から登場している
+		g_enemyLG[i].nShadowIdx = -1;
+
+		g_nCnt1 = 0;					//
+		g_nCnt2 = 0;					//
+		g_nCnt3 = 0;
+		g_nCnt4 = 0;
+
+		g_enemy[i].nStopTime = 0; //最初は動ける
+
+		g_enemy[i].nShadowIdx = CreateShadow(g_enemy[i].pos, 20.0f);
 	}
 	
 	bfCnt = 0;
@@ -91,16 +143,27 @@ HRESULT InitEnemy(void)
 	for (int i = 0; i < ENEMY_MAX; i++)
 	{
 		XMStoreFloat4x4(&g_enemy[i].mtxWorld, mtxWorld);
+		XMStoreFloat4x4(&g_enemyHD[i].mtxWorld, mtxWorld);
+		XMStoreFloat4x4(&g_enemyAM[i].mtxWorld, mtxWorld);
+		XMStoreFloat4x4(&g_enemyLG[i].mtxWorld, mtxWorld);
+
 	}
 
 	// モデルデータの読み込み
-	hr = g_model[0].Load(pDevice, pDeviceContext,MODEL_ENEMY);
-	// モデルデータの読み込み
-	hr = g_model[1].Load(pDevice, pDeviceContext,MODEL_ENEMY01);
-	// モデルデータの読み込み
-	hr = g_model[2].Load(pDevice, pDeviceContext, MODEL_ENEMY02);
-	
+	hr = g_model[0].Load(pDevice, pDeviceContext,
+		MODEL_ENEMY); if (FAILED(hr)) return hr;
+
+	hr = g_model[1].Load(pDevice, pDeviceContext,
+		MODEL_ENEMY1); if (FAILED(hr)) return hr;
+
+	hr = g_model[2].Load(pDevice, pDeviceContext,
+		MODEL_ENEMY2); if (FAILED(hr)) return hr;
+
+	hr = g_model[3].Load(pDevice, pDeviceContext,
+		MODEL_ENEMY3); if (FAILED(hr)) return hr;
+
 	return hr;
+
 }
 
 //=============================================================================
@@ -118,6 +181,7 @@ void UninitEnemy(void)
 	g_model[0].Release();
 	g_model[1].Release();
 	g_model[2].Release();
+	g_model[3].Release();
 }
 
 //=============================================================================
@@ -131,31 +195,54 @@ void UpdateEnemy(void)
 		//未使用
 		if (g_enemy[i].nState == 0) continue;
 
-		//画面外に行かない処理
-		XMFLOAT3 box = g_model[0].GetBBox();
-		//[左端] 
-		if (g_enemy[i].pos.x < -200 + box.x * g_enemy[i].scl.x)
+		if (GetKeyPress(VK_Z))
 		{
-			g_enemy[i].pos.x = -200 + box.x * g_enemy[i].scl.x;
-			g_enemy[i].vel.x *= -1.0f;
+			//g_enemy[i].rot.y+=5;
+			g_enemyHD[i].rot.z += 5;
+			//g_enemyAM[i].rot.x += 5;
+			g_enemyLG[i].rot.x += 5;
+
 		}
-		//[右端]
-		else if (g_enemy[i].pos.x > 200 - box.x * g_enemy[i].scl.x)
+
+
+		if (GetKeyPress(VK_7))
 		{
-			g_enemy[i].pos.x = 200 - box.x * g_enemy[i].scl.x;
-			g_enemy[i].vel.x *= -1.0f;
+			g_nCnt1++;
+			g_enemy[i].pos.z = 30;
+			g_enemy[i].rot.x = 30;
 		}
-		//[下端] プレイヤーの左端＜画面の上端
-		if (g_enemy[i].pos.z > 200 - box.z * g_enemy[i].scl.z)
+
+
+
+
+		g_nCnt4++;
+		if (GetKeyPress(VK_0))//負けアニメーション
 		{
-			g_enemy[i].pos.z = 200 - box.z * g_enemy[i].scl.z;
-			g_enemy[i].vel.z *= -1.0f;
+			g_enemy[i].rot.z = 180;
+			if (g_nCnt4 >= 20)
+			{
+				g_enemyHD[i].pos.z -= 0.05f;
+				g_enemyHD[i].rot.z -= 1.0f;
+				g_enemyHD[i].rot.y -= 1.0f;
+
+				g_enemyAM[i].pos.x -= 0.05f;
+				g_enemyAM[i].rot.z -= 1.0f;
+				g_enemyAM[i].rot.y -= 1.0f;
+
+				g_enemyLG[i].pos.z += 0.05f;
+				g_enemyLG[i].rot.z -= 1.0f;
+				g_enemyLG[i].rot.y -= 1.0f;
+			}
+
 		}
-		//[上端] プレイヤーの左端＜画面の下端
-		else if (g_enemy[i].pos.z < -200 + box.z * g_enemy[i].scl.z)
+
+
+
+
+		//ゲージの動きの処理
+		if (g_enemy[i].nEGauge >= g_enemy[i].nHP / 10)
 		{
-			g_enemy[i].pos.z = -200 + box.z * g_enemy[i].scl.z;
-			g_enemy[i].vel.z *= -1.0f;
+			g_enemy[i].nEGauge--;
 		}
 
 		if (GetKeyTrigger(VK_F))
@@ -174,6 +261,8 @@ void UpdateEnemy(void)
 		mtxScl = XMMatrixScaling(g_enemy[i].scl.x, g_enemy[i].scl.y, g_enemy[i].scl.z);
 		mtxWorld = XMMatrixMultiply(mtxWorld, mtxScl);
 
+
+
 		// 回転を反映
 		mtxRot = XMMatrixRotationRollPitchYaw(XMConvertToRadians(g_enemy[i].rot.x), XMConvertToRadians(g_enemy[i].rot.y + 180), XMConvertToRadians(g_enemy[i].rot.z));
 		mtxWorld = XMMatrixMultiply(mtxWorld, mtxRot);
@@ -183,11 +272,98 @@ void UpdateEnemy(void)
 		mtxTranslate = XMMatrixTranslation(g_enemy[i].pos.x, g_enemy[i].pos.y, g_enemy[i].pos.z);
 		mtxWorld = XMMatrixMultiply(mtxWorld, mtxTranslate);
 
+
 		// ワールドマトリックス設定
 		XMStoreFloat4x4(&g_enemy[i].mtxWorld, mtxWorld);
 
 		//影の移動
 		MoveShadow(g_enemy[i].nShadowIdx, g_enemy[i].pos);
+
+
+		//************************************************:
+		//頭
+
+		XMMATRIX mtxWorldHD;
+		// ワールドマトリックスの初期化
+		mtxWorldHD = XMMatrixIdentity();
+		// スケールを反映
+		mtxScl = XMMatrixScaling(g_enemyHD[i].scl.x, g_enemyHD[i].scl.y, g_enemyHD[i].scl.z);
+		mtxWorldHD = XMMatrixMultiply(mtxWorldHD, mtxScl);
+		// 回転を反映
+		mtxRot = XMMatrixRotationRollPitchYaw(
+			XMConvertToRadians(g_enemyHD[i].rot.x),
+			XMConvertToRadians(g_enemyHD[i].rot.y),//+180
+			XMConvertToRadians(g_enemyHD[i].rot.z)
+		);
+		mtxWorldHD = XMMatrixMultiply(mtxWorldHD, mtxRot);
+
+		// 移動を反映
+		mtxTranslate = XMMatrixTranslation(g_enemyHD[i].pos.x, g_enemyHD[i].pos.y, g_enemyHD[i].pos.z);
+
+		mtxWorldHD = XMMatrixMultiply(mtxWorldHD, mtxTranslate);
+
+		//親の行列を掛ける
+		mtxWorldHD = XMMatrixMultiply(mtxWorldHD, mtxWorld);
+
+		// ワールドマトリックス設定
+		XMStoreFloat4x4(&g_enemyHD[i].mtxWorld, mtxWorldHD);
+
+
+		//************************************************:
+		//腕
+
+		XMMATRIX mtxWorldAM;
+		// ワールドマトリックスの初期化
+		mtxWorldAM = XMMatrixIdentity();
+		// スケールを反映
+		mtxScl = XMMatrixScaling(g_enemyAM[i].scl.x, g_enemyAM[i].scl.y, g_enemyAM[i].scl.z);
+		mtxWorldAM = XMMatrixMultiply(mtxWorldAM, mtxScl);
+		// 回転を反映
+		mtxRot = XMMatrixRotationRollPitchYaw(
+			XMConvertToRadians(g_enemyAM[i].rot.x),
+			XMConvertToRadians(g_enemyAM[i].rot.y),//+180
+			XMConvertToRadians(g_enemyAM[i].rot.z)
+		);
+		mtxWorldAM = XMMatrixMultiply(mtxWorldAM, mtxRot);
+
+		// 移動を反映
+		mtxTranslate = XMMatrixTranslation(g_enemyAM[i].pos.x, g_enemyAM[i].pos.y, g_enemyAM[i].pos.z);
+
+		mtxWorldAM = XMMatrixMultiply(mtxWorldAM, mtxTranslate);
+
+		//親の行列を掛ける
+		mtxWorldAM = XMMatrixMultiply(mtxWorldAM, mtxWorld);
+
+		// ワールドマトリックス設定
+		XMStoreFloat4x4(&g_enemyAM[i].mtxWorld, mtxWorldAM);
+
+		//************************************************:
+		//足
+
+		XMMATRIX mtxWorldLG;
+		// ワールドマトリックスの初期化
+		mtxWorldLG = XMMatrixIdentity();
+		// スケールを反映
+		mtxScl = XMMatrixScaling(g_enemyLG[i].scl.x, g_enemyLG[i].scl.y, g_enemyLG[i].scl.z);
+		mtxWorldLG = XMMatrixMultiply(mtxWorldLG, mtxScl);
+		// 回転を反映
+		mtxRot = XMMatrixRotationRollPitchYaw(
+			XMConvertToRadians(g_enemyLG[i].rot.x),
+			XMConvertToRadians(g_enemyLG[i].rot.y),//+180
+			XMConvertToRadians(g_enemyLG[i].rot.z)
+		);
+		mtxWorldLG = XMMatrixMultiply(mtxWorldLG, mtxRot);
+
+		// 移動を反映
+		mtxTranslate = XMMatrixTranslation(g_enemyLG[i].pos.x, g_enemyLG[i].pos.y, g_enemyLG[i].pos.z);
+
+		mtxWorldLG = XMMatrixMultiply(mtxWorldLG, mtxTranslate);
+
+		//親の行列を掛ける
+		mtxWorldLG = XMMatrixMultiply(mtxWorldLG, mtxWorld);
+
+		// ワールドマトリックス設定
+		XMStoreFloat4x4(&g_enemyLG[i].mtxWorld, mtxWorldLG);
 	}
 
 
@@ -205,24 +381,11 @@ void DrawEnemy(void)
 		//未使用
 		if (g_enemy[i].nState == 0) continue;
 
-        // モデル表示
-		switch (g_enemy[i].Tipe)
-		{  //方向で速度を変える
-		case 0:
-		{
-			g_model[0].Draw(pDeviceContext, g_enemy[i].mtxWorld);
-
-		} break;
-		case 1:
-		{
-			g_model[1].Draw(pDeviceContext, g_enemy[i].mtxWorld);
-		} break;
-		case 2:
-		{
-			g_model[2].Draw(pDeviceContext, g_enemy[i].mtxWorld); 
-		} break;
-		
-		}
+		// モデル表示
+		g_model[0].Draw(pDeviceContext, g_enemy[i].mtxWorld);
+		g_model[1].Draw(pDeviceContext, g_enemyHD[i].mtxWorld);
+		g_model[2].Draw(pDeviceContext, g_enemyAM[i].mtxWorld);
+		g_model[3].Draw(pDeviceContext, g_enemyLG[i].mtxWorld);
 
 	}
 }
@@ -274,7 +437,7 @@ XMFLOAT3 GetEnemySize(int no)
 }
 
 // 敵の出現
-int SetEnemy(XMFLOAT3 pos,int no)
+/*int SetEnemy(XMFLOAT3 pos,int no)
 {
 	tEnemy* pEnemy = g_enemy;
 	for (int i = 0; i < ENEMY_MAX; ++i, ++pEnemy)
@@ -338,7 +501,7 @@ int SetEnemy(XMFLOAT3 pos,int no)
 	}
 
 	return -1;	// 見つからなかった
-}
+}*/
 
 
 int GetEnemyHp(int no)
@@ -377,4 +540,27 @@ void DestroyEnemy(int no)
 
 	StartFade(SCENE_CLEAR);
 
+}
+void DrawEnemyEGauge()
+{
+	const XMFLOAT2 pos = { ENEMY_EGAUGE_POS_X, ENEMY_EGAUGE_POS_Y };
+
+	DrawEGauge(pos, (float)g_enemy[0].nEGauge / EGAUGE_MAX);
+}
+
+void ResetEPos(int no)
+{
+	if (no < 0 || no >= ENEMY_MAX)	return;
+
+	g_enemy[no].pos = XMFLOAT3(0.0f, 20.0f, 20.0f);
+	g_enemy[no].rot = XMFLOAT3(0.0f, 180.0f, 0.0f);
+	g_enemy[no].scl = XMFLOAT3(8.0f, 8.0f, 8.0f);
+	g_enemy[no].vel = XMFLOAT3(0.0f, 0.0f, 0.0f);
+	g_enemyHD[no].pos = XMFLOAT3(0.0f, 0.0f, -3.0f);
+	g_enemyHD[no].rot = XMFLOAT3(0.0f, 0.0f, 0.0f);
+	g_enemyHD[no].scl = XMFLOAT3(1.0f, 1.0f, 1.0f);
+	g_enemyHD[no].vel = XMFLOAT3(0.0f, 0.0f, 0.0f);
+
+	g_nCnt1 = 0;
+	g_nCnt2 = 0;
 }
